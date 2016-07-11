ce91d4e8729b988b37ff8137c15538aabb374d17
Costa <COSTAL@hbm.com>
2016-07-11 14:28:23 +0100

    Added Learn Python The Hard Way

---
+++ Learn Python The Hard Way/ex6.py
@@ -0,0 +1,39 @@
+
+# Create a string that says how many types of ppl there are
+x = "There are %d types of people." % 10
+
+# make var binary
+binary = "binary"
+
+# make var do_not
+do_not = "don't"
+
+# make a string, input the binary and do_not vars
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+# print them
+print x
+print y
+
+# print a string, then input the raw value of x
+print "I said: %r" % x
+
+# print a string and input the string value of y
+print "I also said: '%s'." % y
+
+# var hilarious = bool false
+hilarious = False
+
+# print a string with the raw value of a given input
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+# print the joke_evaluation string with the hilarious iinput
+print joke_evaluation % hilarious
+
+# generate two strings
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# concatenate them
+print w + e
+

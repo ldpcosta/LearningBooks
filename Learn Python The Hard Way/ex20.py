606ef5e0dfa084516adb6590ce6e8ff00b1ef40b
Costa <COSTAL@hbm.com>
2016-07-13 17:06:53 +0100

    Finished first half.

--- Learn Python The Hard Way/ex20.py
+++ Learn Python The Hard Way/ex20.py
@@ -1,18 +1,28 @@
 ### exercise 20
 
+# Import argv from sys library
 from sys import argv
 
+# Take the input_file from shell arguments
 script, input_file = argv
 
-def print_all(f)
+#define funciton print_all, which prints the whole document to the screen
+def print_all(f):
 	print f.read()
 
-def rewind(f).
+#define function rewind, which sets the "cursor"
+# to the start of the file
+def rewind(f):
 	f.seek(0)
 
+
+#print the next line in the file, in order, relative to the last one
 def print_a_line(line_count,f):
 	print line_count, f.readline()
 
+
+
+# Opening the file, numbering the lines and other stuff.
 current_file = open(input_file)
 
 print "First let's print the whole file:\n"
